c11554010261806504fd659234b91a187ee52eb1
Mateus Soares <mateussoaress1997@gmail.com>
2021-08-11 20:38:21 -0300

    Add hello world as json string

--- src/main/java/com/example/app/HelloWorldController.java
+++ src/main/java/com/example/app/HelloWorldController.java
@@ -4,11 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.AbstractMap;
+import java.util.HashMap;
+
 @RestController
 public class HelloWorldController {
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return "Hello world!";
+    public AbstractMap<String, String> greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("message", "Hello world!");
+        return map;
     }
 }
